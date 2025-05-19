0f16667e26b29c62950ca182669ad68b3b646d6c
mohammedmir96 <116528020+mohammedmir96@users.noreply.github.com>
2025-05-18 22:40:36 -0500

    Add files via upload

---
+++ trie_template.py
@@ -0,0 +1,36 @@
+""" 
+Project 2: Trie
+Example starter program
+author: Dr. Eric Chou
+Date: 12/25/2021
+"""
+class Trie: 
+    def __init__(self): # To be implemented by students
+        pass
+
+    def size(self): # To be implemented by students
+        pass 
+
+    def is_empty(self): # To be implemented by students
+        pass 
+
+    def add(self, word): # To be implemented by students
+        pass 
+
+    def get(self, word):  # To be implemented by students
+        pass 
+
+    def clear(self, word): # to be implemented by students
+        pass 
+
+    def __repr__(self): # To be implemented by students
+        pass 
+
+    def __str__(self): # To be implemented by students
+        pass 
+
+def main(): # To be implemented by students
+    pass
+
+if __name__ == "__main__": # To be implemented by students
+    main()
